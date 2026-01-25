c7fcac39b7dca45a79d362c6bd3be7df88fae43a
V0dis <129592118+V0dis@users.noreply.github.com>
2026-01-25 03:43:37 +0300

    Initial

---
+++ Assets/Scripts/House/AlarmSound.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+public class AlarmSound : MonoBehaviour
+{
+    [SerializeField] private CollisionDetector _collisionDetector;
+    [SerializeField] private AudioSource _alarmAudio;
+    [SerializeField] private float _maxVolume = 1f;
+    [SerializeField] private float _fadeSpeed = 0.25f;
+    
+    private float _minVolume = 0f;
+    private bool _isActive = false;
+
+    private void OnEnable()
+    {
+        if (_alarmAudio == null)
+            return;
+        
+        _collisionDetector.OnIntruderEntered += SetAlarm;
+    }
+
+    private void Update()
+    {
+        if (_isActive)
+            _alarmAudio.volume = Mathf.MoveTowards(_alarmAudio.volume, _maxVolume, _fadeSpeed * Time.deltaTime);
+        else
+            _alarmAudio.volume = Mathf.MoveTowards(_alarmAudio.volume, _minVolume, _fadeSpeed * Time.deltaTime);
+    }
+
+    private void OnDisable()
+    {
+        _collisionDetector.OnIntruderEntered -= SetAlarm;
+    }
+    
+    private void SetAlarm(bool isEntered) => _isActive = isEntered;
+}
---
+++ Assets/Scripts/House/CollisionDetector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class CollisionDetector : MonoBehaviour
+{
+    public event Action<bool> OnIntruderEntered;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.GetComponent<Enemy>() != null)
+            OnIntruderEntered?.Invoke(true);
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.GetComponent<Enemy>() != null) 
+            OnIntruderEntered?.Invoke(false);
+    }
+}
